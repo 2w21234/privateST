f7ebe35c7624c2562d7b9fb51b1cd8fb0fc7679c
Hakin <hakin@snu.ac.kr>
2026-01-23 22:21:50 +0900

    Add test script for privateST

--- test_privateST_1.py
+++ test_privateST_1.py
@@ -57,18 +57,18 @@ torch.use_deterministic_algorithms(True)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-model_ckpt         = "./model/epoch_11_model_state_dict.pth"
-train_count_root    = "./training/counts/512/Breast_cancer"
-train_img_root      = "./training/images/512/Breast_cancer"
+model_ckpt         = "./model/epoch_15_model_state_dict.pth"
+train_count_root    = "./training/counts/224/Breast_cancer"
+train_img_root      = "./training/images/224/Breast_cancer"
 
 test_patients_csv  = "./test/test_patients.csv"
-test_count_root    = "./test/counts/512/Breast_cancer"
-test_img_root      = "./test/images/64"
+test_count_root    = "./test/counts/224/Breast_cancer"
+test_img_root      = "./test/images/224/Breast_cancer/BC23287/"
 gene_filter        = 250
 orion_batch_size   = 1
 resol              = 64
 device             = torch.device("cpu")
-precomputed_root   = "./precomputed_stats"
+precomputed_root   = "./model/precomputed_stats"
 save_dir = "./results"
 
 
@@ -175,8 +175,115 @@ print("Orion model loaded.")
 
 
 
-
 class Spatial(torch.utils.data.Dataset):
+    def __init__(self,
+                 patient=None,
+                 count_root=None,
+                 img_root=None,
+                 resolution=64,  # resolution 인자 추가
+                 gene_filter=250, # gene_filter 기본값 수정 (변수 사용시 주의)
+                 aux_ratio=0,
+                 transform=None,
+                 normalization=None,
+                 gene_info_root=None):
+        
+        # 데이터셋 경로 로드
+        self.dataset = glob.glob(os.path.join(count_root, "*", "*.npz"))
+
+        if patient is not None:
+             self.dataset = [d for d in self.dataset if ((d.split("/")[-2] in patient) or ((d.split("/")[-2], d.split("/")[-1].split("_")[0]) in patient))]
+
+        self.transform = transform
+        self.count_root = count_root
+        self.img_root = img_root
+        self.resolution = resolution  # 해상도 저장
+        self.gene_filter = gene_filter
+        self.aux_ratio = aux_ratio
+        self.normalization = normalization
+        self.gene_info_root = gene_info_root
+
+        # 유전자 정보 로드
+        gene_pkl_path = os.path.join(self.gene_info_root, "gene.pkl")
+        mean_expression_path = os.path.join(self.gene_info_root, "mean_expression.npy")
+
+        print('gene_pkl_path : ', gene_pkl_path)
+        print('mean_expression_path :', mean_expression_path)
+        
+        with open(gene_pkl_path, "rb") as f:
+            self.ensg_names = pickle.load(f)
+        self.mean_expression = np.load(mean_expression_path)
+
+        self.gene_names = list(map(lambda x: ensembl.symbol[x], self.ensg_names))
+
+        # Gene Filtering Logic
+        keep_gene = set(list(zip(*sorted(zip(self.mean_expression, range(self.mean_expression.shape[0])))[::-1][:self.gene_filter]))[1])
+        self.keep_bool = np.array([i in keep_gene for i in range(len(self.gene_names))])
+
+        self.ensg_keep = [n for (n, f) in zip(self.ensg_names, self.keep_bool) if f]
+        self.gene_keep = [n for (n, f) in zip(self.gene_names, self.keep_bool) if f]
+
+        if self.aux_ratio != 0:
+            self.aux_nums = int((len(self.gene_names) - self.gene_filter) * self.aux_ratio)
+            aux_gene = set(list(zip(*sorted(zip(self.mean_expression, range(self.mean_expression.shape[0])))[::-1][self.gene_filter:self.gene_filter + self.aux_nums]))[1])
+            self.aux_bool = np.array([i in aux_gene for i in range(len(self.gene_names))])
+            self.ensg_aux = [n for (n, f) in zip(self.ensg_names, self.aux_bool) if f]
+            self.gene_aux = [n for (n, f) in zip(self.gene_names, self.aux_bool) if f]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        npz = np.load(self.dataset[index])
+        count = npz["count"]
+        pixel = npz["pixel"]
+        patient = npz["patient"][0]
+        section = npz["section"][0]
+        coord = npz["index"]
+
+        # [수정됨] .jpg 이미지 경로 생성 (패딩 제거: 0010 -> 10)
+        # 예: input_tensor_... 대신 C1_10_17.jpg 형식으로 변경
+        img_filename = f"{section}_{coord[0]}_{coord[1]}.jpg"
+        
+        # 경로 결합: img_root/patient/filename
+        # (img_root 설정에 따라 patient 폴더가 포함되어 있는지 확인 필요)
+        img_path = os.path.join(self.img_root, patient, img_filename)
+
+        # 이미지 로드 (PIL 사용)
+        try:
+            X = Image.open(img_path).convert("RGB")
+        except FileNotFoundError:
+            # 혹시 경로가 틀렸을 경우 디버깅용 (필요시 주석 처리)
+            # print(f"File not found: {img_path}, trying without patient folder...")
+            img_path_alt = os.path.join(self.img_root, img_filename)
+            X = Image.open(img_path_alt).convert("RGB")
+
+        # Transform 적용
+        if self.transform is not None:
+            X = self.transform(X)
+
+        # Resize 적용 (Spatial_train과 동일하게 맞춤)
+        if X.shape[1] != self.resolution:
+            X = torchvision.transforms.Resize((self.resolution, self.resolution))(X)
+
+        coord = torch.as_tensor(coord)
+        index = torch.as_tensor([index])
+
+        keep_count = count[self.keep_bool]
+        y = torch.as_tensor(keep_count, dtype=torch.float)
+        y = torch.log(1 + y)
+
+        if self.normalization is not None:
+            y = (y - self.normalization[0]) / self.normalization[1]
+
+        if self.aux_ratio != 0:
+            aux_count = count[self.aux_bool]
+            aux = torch.as_tensor(aux_count, dtype=torch.float)
+            aux = torch.log(1 + aux)
+            return X, y, aux, coord, index, patient, section, pixel
+        else:
+            return X, y, coord, index, patient, section, pixel
+
+class Spatial_1(torch.utils.data.Dataset):
     def __init__(self,
                  patient=None,
                  count_root=None,
@@ -615,4 +722,4 @@ print("="*50)
 print(f"Number of samples : {len(sample_times)}")
 print('Inference times :', sample_times)
 print("="*50)
-~                      
+                      
